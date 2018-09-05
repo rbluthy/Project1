1b10bc9e6f0d72c75a3c46d8d500eabbea07b358
rbluthy <rbluthy@go.olemiss.edu>
2018-09-04 20:13:54 -0500

    Done

--- project1.js
+++ project1.js
@@ -1,3 +1,11 @@
 
 alert("Hello! Thank you for visiting the Phillies Fan Page");
 
+
+
+
+const changeText = () => {
+    const h5 = document.querySelector('h5');
+
+    h5.textContent = "Current Homerun leader is Rhys Hoskins";
+}
